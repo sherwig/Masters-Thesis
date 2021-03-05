e8e833e8e76e2d5b1a6da0770297df9fb29f965d
Sam <>
2021-03-05 10:52:34 -0700

    Waves in speed shader

--- src/shaders/render/fragment.glsl
+++ src/shaders/render/fragment.glsl
@@ -9,20 +9,21 @@ precision highp float;
   varying float vElevation;
 
 
-  // void main() {
-  //   // tint the particle texture but keep the particle texture alpha
-  //   vec4 diffuseColor = texture2D( map, vUv);
-  //   // vec4 diffuseColor2 = texture2D( colorMap, vUv );
-  //   vec4 diffuseColor2 = texture2D( colorMap, vColorUV);
-  //   gl_FragColor = vec4(diffuseColor2.rgb, diffuseColor.a);
-  // }
-
-
-
-  void main()
-  {
-    float mixStrength = (vElevation+0.08)*5.0;
-    vec3 color = mix(uDepthColor,uSurfaceColor,mixStrength);
-    gl_FragColor = vec4(color,1.0);
-
+  void main() {
+    // tint the particle texture but keep the particle texture alpha
+    vec4 diffuseColor = texture2D( map, vUv);
+    // vec4 diffuseColor2 = texture2D( colorMap, vUv );
+    vec4 diffuseColor2 = texture2D( colorMap, vColorUV);
+    gl_FragColor = vec4(diffuseColor2.rgb, diffuseColor.a);
   }
+
+
+
+  // void main()
+  // {
+  //   float mixStrength = (vElevation+0.08)*5.0;
+  //   vec4 diffuseColor = texture2D( map, vUv);
+  //   vec3 color = mix(uDepthColor,uSurfaceColor,mixStrength);
+  //   gl_FragColor = vec4(color,diffuseColor.a);
+  //
+  // }
